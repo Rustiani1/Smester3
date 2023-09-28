b782ce934378d17922db737836e36cbb8392cc6b
Rustiani1 <123487139+Rustiani1@users.noreply.github.com>
2023-09-28 22:54:47 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,12 @@
+let NAMA = "RUSTIANI";
+let NIM = "20220040003";
+
+console.log("Nama: " + NAMA);
+console.log("NIM: " + NIM);
+
+let angka1 = 7;
+let angka2 = 8;
+
+let jumlah = angka1 + angka2;
+console.log("hasil penjumlahan: " + jumlah);
+
